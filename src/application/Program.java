bbc7dadbc99e9ab025c7431cd34416db01ec0da4
Laura Perlingeiro <laura.bentper@gmail.com>
2024-09-07 01:01:51 -0300

    creating the Position, Piece and Board classes

---
+++ src/application/Program.java
@@ -0,0 +1,9 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+        Position p = new Position(0, 0);
+    }
+}
---
+++ src/boardgame/Board.java
@@ -0,0 +1,17 @@
+package boardgame;
+
+public class Board {
+    private int rows;
+    private int cols;
+    private Piece[][] pieces;
+
+    public Board(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+        this.pieces = new Piece[rows][cols];
+    }
+    public int getRows() {return rows;}
+    public int getCols() {return cols;}
+    public void setCols(int cols) {this.cols = cols;}
+    public void setRows(int rows) {this.rows = rows;}
+}
