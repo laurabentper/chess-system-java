e0399210f23011195caaecac7eec1796798d917f
Laura Perlingeiro <laura.bentper@gmail.com>
2024-09-07 21:44:44 -0300

    catching exceptions and programming the chess logic (until rook)

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -6,15 +6,63 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int cols) {
+        if(rows < 1 || cols < 1){
+            throw new BoardException("Invalid board length: there must be at least 1 row and 1 column");
+        }
         this.rows = rows;
         this.cols = cols;
         this.pieces = new Piece[rows][cols];
     }
     public int getRows() {return rows;}
     public int getCols() {return cols;}
-    public void setCols(int cols) {this.cols = cols;}
-    public void setRows(int rows) {this.rows = rows;}
 
-    public Piece piece(int row, int col) {return pieces[row][col];}
-    public Piece piece(Position pos) {return pieces[pos.getRow()][pos.getCol()];}
+    public Piece piece(int row, int col) {
+        if(!positionExists(row, col)){
+            throw new BoardException("Position not on the board");
+        }
+        return pieces[row][col];
+    }
+    public Piece piece(Position pos) {
+        if(!positionExists(pos)){
+            throw new BoardException("Position not on the board");
+        }
+        return pieces[pos.getRow()][pos.getCol()];
+    }
+
+    public void placePiece(Piece piece, Position pos) {
+        if(thereIsAPieace(pos)){
+            throw new BoardException("There is already a piece on position " + pos);
+        }
+        pieces[pos.getRow()][pos.getCol()] = piece;
+        piece.position = pos;
+    }
+
+    public Piece removePiece(Position pos) {
+        if(!positionExists(pos)){
+            throw new BoardException("Position not on the board");
+        }
+        if(piece(pos)==null){
+            return null;
+        }
+        Piece aux = piece(pos);
+        aux.position = null;
+        pieces[pos.getRow()][pos.getCol()] = null;
+        return aux;
+    }
+
+    private boolean positionExists(int row, int col) {
+       return  row >= 0 && row < rows && col >= 0 && col < cols;
+    }
+
+    public boolean positionExists(Position pos) {
+        return positionExists(pos.getRow(), pos.getCol());
+    }
+
+    public boolean thereIsAPieace(Position pos) {
+        if(!positionExists(pos)){
+            throw new BoardException("Position not on the board");
+        }
+        return piece(pos) != null;
+    }
+
 }
---
+++ src/chess/pieces/Rook.java
@@ -0,0 +1,67 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Rook extends ChessPiece {
+
+    public Rook(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "R";
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getCols()];
+
+        Position p = new Position(0,0);
+
+        //searching above
+        p.setValues(position.getRow()-1, position.getCol());
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPieace(p)){
+            mat[p.getRow()][p.getCol()] = true;
+            p.setRow(p.getRow()-1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getCol()] = true;
+        }
+
+        //searching left
+        p.setValues(position.getRow(), position.getCol()-1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPieace(p)){
+            mat[p.getRow()][p.getCol()] = true;
+            p.setCol(p.getCol()-1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getCol()] = true;
+        }
+
+        //searching right
+        p.setValues(position.getRow(), position.getCol()+1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPieace(p)){
+            mat[p.getRow()][p.getCol()] = true;
+            p.setCol(p.getCol()+1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getCol()] = true;
+        }
+
+        //searching below
+        p.setValues(position.getRow()+1, position.getCol());
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPieace(p)){
+            mat[p.getRow()][p.getCol()] = true;
+            p.setRow(p.getRow()+1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getCol()] = true;
+        }
+
+        return mat;
+    }
+}
