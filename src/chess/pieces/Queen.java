cb5f7c2f1f5bf6a40c4b9c14c3c2a24722b7dd29
Laura Perlingeiro <laura.bentper@gmail.com>
2024-09-08 17:04:28 -0300

    Queen

---
+++ src/chess/pieces/Queen.java
@@ -0,0 +1,107 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Queen extends ChessPiece {
+
+    public Queen(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "Q";
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getCols()];
+
+        Position p = new Position(0,0);
+
+        //searching above
+        p.setValues(position.getRow()-1, position.getCol());
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPieace(p)){
+            mat[p.getRow()][p.getCol()] = true;
+            p.setRow(p.getRow()-1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getCol()] = true;
+        }
+
+        //searching left
+        p.setValues(position.getRow(), position.getCol()-1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPieace(p)){
+            mat[p.getRow()][p.getCol()] = true;
+            p.setCol(p.getCol()-1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getCol()] = true;
+        }
+
+        //searching right
+        p.setValues(position.getRow(), position.getCol()+1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPieace(p)){
+            mat[p.getRow()][p.getCol()] = true;
+            p.setCol(p.getCol()+1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getCol()] = true;
+        }
+
+        //searching below
+        p.setValues(position.getRow()+1, position.getCol());
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPieace(p)){
+            mat[p.getRow()][p.getCol()] = true;
+            p.setRow(p.getRow()+1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getCol()] = true;
+        }
+
+        //searching northwest
+        p.setValues(position.getRow()-1, position.getCol()-1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPieace(p)){
+            mat[p.getRow()][p.getCol()] = true;
+            p.setValues(p.getRow()-1, p.getCol()-1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getCol()] = true;
+        }
+
+        //searching northeast
+        p.setValues(position.getRow()-1, position.getCol()+1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPieace(p)){
+            mat[p.getRow()][p.getCol()] = true;
+            p.setValues(p.getRow()-1, p.getCol()+1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getCol()] = true;
+        }
+
+        //searching southeast
+        p.setValues(position.getRow()+1, position.getCol()+1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPieace(p)){
+            mat[p.getRow()][p.getCol()] = true;
+            p.setValues(p.getRow()+1, p.getCol()+1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getCol()] = true;
+        }
+
+        //searching southwest
+        p.setValues(position.getRow()+1, position.getCol()-1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPieace(p)){
+            mat[p.getRow()][p.getCol()] = true;
+            p.setValues(p.getRow()+1, position.getCol()-1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getCol()] = true;
+        }
+
+        return mat;
+    }
+}
